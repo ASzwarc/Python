79376d03c2022d59119c23e1c5c49a4d4580877c
mrMunch1988 <szwarc.adam@gmail.com>
2014-06-09 22:09:04 +0200

    Reading file that name was passed as argument. Added example file for testing purposes

--- ASCIIGameOfLife.py
+++ ASCIIGameOfLife.py
@@ -1,1 +1,15 @@
 __author__ = 'Adam'
+
+import sys
+
+if len(sys.argv) > 1 :
+    inputFile = open(str(sys.argv[1]), 'r')
+else :
+    print "You should specify file to open"
+
+for line in inputFile :
+    print line
+
+
+
+
